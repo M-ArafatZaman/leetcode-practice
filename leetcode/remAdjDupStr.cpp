08171e3802ccca2efb53459460a12529e86908c6
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-11-10 23:04:27 +0600

    Basic brute force implementation

---
+++ leetcode/remAdjDupStr.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+
+/**
+ * This is a template file for all leetcode problems
+ * Author: @ Mohammad Arafat Zaman
+*/
+
+using namespace std;
+
+#define DEBUG(x) cout << #x << " = " << x << endl 
+
+template <class T> void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}" << endl; }
+
+template <class T, class S>
+void print_map(map<T, S>& _m) {
+    cout << "{ ";
+    for (auto & n : _m) {
+        cout << n.first << " = " << n.second << ", ";
+    }
+    cout << "\b }" << endl;
+}
+
+#define PI = 3.141592653589793
+
+// https://www.youtube.com/watch?v=5v3wQoRLiY4
+class Solution {
+    public: 
+        string removeDuplicates(string s) {
+            bool removed = true;
+            string newS = s;
+
+            while (removed) {
+                removed = false;
+                string g = remDup(newS);
+                if (g != newS) removed = true;
+                newS = g;
+            }
+
+            return newS;
+        }
+
+        string remDup(string s) {
+            string newString;
+            int n = s.size();
+            int i = 0;
+            for (; i < n-1; ++i) {
+                if (s[i] != s[i+1]) {
+                    newString += s[i];
+                } else {
+                    ++i;
+                }
+            }
+            // Check if any is remaining
+            if (i < n) newString += s[i];
+
+            return newString;
+        }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Run tests
+    Solution solver;
+    string a = "azxxzy";
+
+    cout << solver.removeDuplicates(a);
+
+    
+    return 0;
+}
+
