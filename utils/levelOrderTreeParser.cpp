a7576ccee3461eff1cbf3fd78f59c6698bd5b178
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-09 10:07:41 +0600

    Basic level order tree traversal parser.

---
+++ utils/levelOrderTreeParser.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+/// @brief A function to parse a tree from an array in leetcode style
+/// @param arr - The array
+/// @return - The root tree node
+TreeNode* parseTree(vector<int>& arr) {
+    TreeNode* root = new TreeNode();
+    if (arr.size() == 0) return root;
+    root->val = arr[0];
+
+    // Parse using level order traversal
+    queue<TreeNode*> nodes;
+    nodes.push(root);
+    for (int i = 1; i < arr.size(); ++i) {
+        TreeNode* currNode = nodes.front();
+        nodes.pop();
+        
+        // Parse the current node
+        if (arr[i] != NULL) {
+            TreeNode* leftChild = new TreeNode(arr[i]);
+            currNode->left = leftChild;
+            nodes.push(leftChild);
+        }
+
+        // Pre parse the next node (the right node)
+        if (i + 1 < arr.size() && arr[i++] != NULL) {
+            TreeNode* rightChild = new TreeNode(arr[i]);
+            currNode->right = rightChild;
+            nodes.push(rightChild);
+        }
+    };
+
+    return root;
+};
+
+int main() {
+
+    return 0;
+}
