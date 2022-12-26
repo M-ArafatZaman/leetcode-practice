657ca9a9bebb833dd90489591dbaf6d65abbc51d
M-ArafatZaman <mdarafatzaman1@gmail.com>
2022-12-27 05:33:03 +0600

    Daily challenge

---
+++ leetcode/jumpGame.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+
+/**
+ * This is a template file for all leetcode problems
+ * Author: @ Mohammad Arafat Zaman
+*/
+
+using namespace std;
+
+#define DEBUG(x) cout << #x << " = " << x << endl 
+
+template <class T> void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}" << endl; }
+
+template <class T, class S> void print_map(map<T, S>& _m) { cout << "{ "; for (auto & n : _m) { cout << n.first << " = " << n.second << ", "; } cout << "\b }" << endl; }
+
+#define PI = 3.141592653589793
+
+
+class Solution {
+    public:
+        bool canJump(vector<int>& nums) {
+            int n = nums.size();
+            vector<bool> dp(n, 0);
+
+            if (n == 1) {
+                return true;
+            };
+            
+            int can_reach = nums[0];
+            
+            for (int i = 1; i < n; ++i) {
+                if (can_reach < i) {
+                    return false;
+                }
+                can_reach = max(can_reach, i+nums[i]);
+            }
+
+            return true;
+        }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    // Run tests
+    Solution solver;
+    vector<int> a = {2,3,1,1,4};
+
+    cout << solver.canJump(a);
+
+
+    
+    return 0;
+}
+
