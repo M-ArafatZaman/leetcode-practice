7cfca5697ede1c5257a9b98c19d513f688790a16
M-ArafatZaman <mdarafatzaman1@gmail.com>
2023-12-26 15:37:23 -0330

    Neetcode stack roadmap O(1)

--- leetcode/minStack.py
+++ leetcode/minStack.py
@@ -1,26 +1,23 @@
-import heapq
-
 # https://leetcode.com/problems/min-stack/
 class MinStack:
 
     def __init__(self):
         self.stk = []
-        self.curr_min = 2**33
+        self.minStk = []
 
     def push(self, val: int) -> None:
         self.stk.append(val)
-        self.curr_min = min(self.curr_min, val)
+        self.minStk.append( min(val, self.minStk[-1] if len(self.minStk) > 0 else val) )
 
     def pop(self) -> None:
-        rem = self.stk.pop()
-        if rem == self.curr_min:
-            self.curr_min = min(self.stk) if len(self.stk) > 0 else 2**33
+        self.stk.pop()
+        self.minStk.pop()
 
     def top(self) -> int:
         return self.stk[-1]
 
     def getMin(self) -> int:
-        return self.curr_min
+        return self.minStk[-1]
 
 
 # Your MinStack object will be instantiated and called as such:
